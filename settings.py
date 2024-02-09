4c9feaf0a4e012765579c88847ab63c7c868242e
GachiVlad <gachivladinerrad1337@gmail.com>
2024-02-09 21:23:35 +0300

    База
    Намечена структура кода, создана мини-карта и вывод игры на экран пользователя, а также выход из нее.

---
+++ settings.py
@@ -0,0 +1,9 @@
+""""В этом файле содержатся базовые настройки игры"""
+
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 60
+
+PLAYER_POS = 1.5, 5 # Position on mini map
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002

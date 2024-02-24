e16c9ef24ad918b8c07768b60173c35bc4924bec
GachiVlad <gachivladinerrad1337@gmail.com>
2024-02-24 15:45:09 +0300

    Changed the key concept of game
    The original concept was too hard, so we decided to make a different game

--- settings.py
+++
@@ -1,8 +0,0 @@
-""""В этом файле содержатся базовые настройки игры"""
-
-RES = WIDTH, HEIGHT = 1600, 900
-FPS = 60
-
-PLAYER_POS = 1.5, 5 # Position on mini map
-PLAYER_ANGLE = 0
-PLAYER_SPEED = 0.004
